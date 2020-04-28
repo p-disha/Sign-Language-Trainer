052b3d821018a2dd37a23b4bfebe8b525f6d709b
harshul1610 <harshulrobo@gmail.com>
2020-04-27 22:04:08 -0400

    added the handtrack.js

--- flask_webapp/app.py
+++ flask_webapp/app.py
@@ -9,5 +9,9 @@ app = Flask(__name__)
 def index_view():
     return render_template('index.html')
 
+@app.route('/train')
+def train_view():
+    return render_template('train.html')
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
