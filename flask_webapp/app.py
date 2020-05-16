1b4c1fb8664b968a114b04fe631a3d8c36acbb3c
harshul1610 <harshulrobo@gmail.com>
2020-05-16 10:53:48 -0400

    adding the letter image in the div

--- flask_webapp/app.py
+++ flask_webapp/app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask import render_template
-
+from flask import send_from_directory
 
 app = Flask(__name__)
 
@@ -18,4 +18,4 @@ def test_view():
     return render_template('test.html')
     
 if __name__ == '__main__':
-    app.run(debug=True, port=5001)
+    app.run(debug=True, port=5201)
